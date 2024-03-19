e1772ca62fd80742a031363dd1d1ad6af413dfe4
Baraka <barakamwenda1@gmail.com>
2024-03-19 19:24:04 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import TodoList from "./Components/TodoList";
 function App() {
   return (
     <>
-      <div className="App">
+      <div className="App" id="app">
         <h1>What's The plan for Today?</h1>
         <TodoList />
       </div>
